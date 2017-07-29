d0a1ba7c42a6c37320ac0e8b7962b3bf8415489e
thedashdude <thedashdude@yahoo.com>
2017-07-29 10:38:39 -0400

    Moved plat main to subfolder, started world.

---
+++ world.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class World():
+    def __init__(self,EntityList,screen):
+        self.entities = EntityList
+        self.screen = screen
+    def drawAll(self):
+        for k in self.entities:
+            k.draw(self.screen)
+    def update(self,keystate,mousePos,mousePressed):
+        boxes = []
+        ents = []
+        for k in self.entities:
+            k.update(keystate,mousePos,mousePressed)
+            box.append(k.rect)
+            ents.append(k)
+        ents = np.array(ents)
+        for k in self.entities:
+            k.collide( ents[k.rect.collidelistall(boxes)] )
+            for entity in k.get_new_entities():
+                self.entities.append(entity)
