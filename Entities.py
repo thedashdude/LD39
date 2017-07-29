2650f0f02b16e7321d1f171c397f00d02f86e1eb
deltaalpha33 <dalexakos@gmail.com>
2017-07-29 11:08:13 -0400

    updated file

--- Entities.py
+++ Entities.py
@@ -3,26 +3,30 @@ import pygame
 from pygame.locals import *
 
 class Entity():
-	def __init__(self,current_world, x=0, y=0):
+	def __init__(self,current_world, x=0, y=0, length = 32, width = 32):
 		self.x = 0
 		self.y = 0
 		self.current_world = current_world
+		self.rect = Rect(self.x,self.y,length,width)
+
+		self.new_entities = None
 
 	def move(self,x,y):
 		self.x += x
 		self.y += y
-		self.rect = Rect(self.x,self.y,32,32)
+		
 
-	def update(self, current_input):
+	def update(self, keystate, mouse_position, mouse_press, collided_entites):
 		pass
 
+	def get_new_entities(self):
+		x = self.new_entities
+		self.new_entities = None
+		return x
 
-	def get_collision_box(self,vcollision_object):
-		pass
+	def get_collision_box(self):
+		return self.rect
 
 	def draw(self, screen):
 		pass
 
-
-class Shape():
-	def __init__():
