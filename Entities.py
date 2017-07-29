7af33e45fcdc580e34e6f36d9af57b344d58838f
deltaalpha33 <dalexakos@gmail.com>
2017-07-29 13:12:37 -0400

    added collision detection

--- Entities.py
+++ Entities.py
@@ -3,35 +3,59 @@ import pygame
 from pygame.locals import *
 
 class Entity():
-    def __init__(self, x=0, y=0, length = 32, width = 32, texture_loc = "textures/test.jpg"):
-        self.rect = Rect(x,y,length,width)
+    def __init__(self, x=0, y=0, width = 32, height = 32, texture_loc = "textures/test.jpg"):
+        self.rect = Rect(x,y,width,height)
 
-        self.new_entities = []
+        self.new_entities = list()
 
         self.texture = pygame.image.load(texture_loc)
 
     def update(self, keystate, mouse_position, mouse_press, collided_entites):
         pass
 
+    def prevent_intersection(self, entity):
+        pass
+
     def get_new_entities(self):
         x = self.new_entities
-        self.new_entities = []
+        self.new_entities = list()
         return x
-
-    def get_collision_box(self):
-        return self.rect
 
     def draw(self, screen):
         
-        scaled_texture = pygame.transform.scale(myimage, (self.rect.length, self.rect.width))
+        scaled_texture = pygame.transform.scale(self.texture, (self.rect.width, self.rect.height))
         screen.blit(scaled_texture, self.rect)
 
 class Test(Entity):
     def __init__(self,x=0,y=0):
         super().__init__(x,y)
+
+
     def update(self, keystate, mouse_position, mouse_press, collided_entites):
-        self.rect.top = self.rect.top + 1
-        if keystate[K_UP]:
-            self.rect.top = self.rect.top - 2
+        pass
     # def draw(self, screen):
     #   pygame.draw.rect(screen, (200,200,200), self.rect)
+
+
+
+class Player(Entity):
+    def __init__(self, x=0, y=0):
+        super().__init__(x,y)
+
+    def update(self, keystate, mouse_position, mouse_press, collided_entites):
+        if keystate[K_UP]:
+            self.rect.top = self.rect.top - 2
+
+        if keystate[K_DOWN]:
+            self.rect.top = self.rect.top  + 2
+
+        if keystate[K_LEFT]:
+            self.rect.left = self.rect.left - 2
+
+        if keystate[K_RIGHT]:
+            self.rect.left = self.rect.left  + 2
+
+        for entity in collided_entites:
+            print("collided")
+            self.prevent_intersection(entity)
+        print("loop")
--- world.py
+++ world.py
@@ -4,21 +4,34 @@ class World():
     def __init__(self,EntityList,screen):
         self.entities = EntityList
         self.screen = screen
+
+        self.collisions_array = list()
+
     def draw_all(self):
         for k in self.entities:
             k.draw(self.screen)
 
     def update(self,keystate,mousePos,mousePressed):
-        boxes = []
-        ents = []
-        for k in self.entities:
-            boxes.append(k.rect)
-            ents.append(k)
-        ents = np.array(ents)
-        for k in self.entities:
+        self.calculate_collisions()
+        for index, entity in enumerate(self.entities):
 
-            k.update(keystate,mousePos,mousePressed, ents[k.rect.collidelistall(boxes)] )
-            for entity in k.get_new_entities():
-                self.entities.append(entity)
+            entity.update(keystate,mousePos,mousePressed, self.collisions_array[index] )
+
+            for new_entity in entity.get_new_entities():
+                self.entities.append(new_entity)
+
+    def calculate_collisions(self):
+        self.collisions_array = [
+
+
+        [self.entities[collision_index]  #create list of collided entitites
+        
+
+        for collision_index in entity.rect.collidelistall(self.entities[:entity_index] + self.entities[entity_index+1:] )]  #check if an entity collides with all other entities
+
+
+        for entity_index, entity in enumerate(self.entities)] #get each entity with its index
+
+
     def cont(self):
         return True
