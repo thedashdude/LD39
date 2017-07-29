63b4534870cd22828cf8bdb66cba214e488004d2
deltaalpha33 <dalexakos@gmail.com>
2017-07-29 11:55:29 -0400

    added texture loading

--- Entities.py
+++ Entities.py
@@ -3,32 +3,35 @@ import pygame
 from pygame.locals import *
 
 class Entity():
-	def __init__(self, x=0, y=0, length = 32, width = 32):
-		self.rect = Rect(x,y,length,width)
+    def __init__(self, x=0, y=0, length = 32, width = 32, texture_loc = "textures/test.jpg"):
+        self.rect = Rect(x,y,length,width)
 
-		self.new_entities = []
-		
+        self.new_entities = []
 
-	def update(self, keystate, mouse_position, mouse_press, collided_entites):
-		pass
+        self.texture = pygame.image.load(texture_loc)
 
-	def get_new_entities(self):
-		x = self.new_entities
-		self.new_entities = []
-		return x
+    def update(self, keystate, mouse_position, mouse_press, collided_entites):
+        pass
 
-	def get_collision_box(self):
-		return self.rect
+    def get_new_entities(self):
+        x = self.new_entities
+        self.new_entities = []
+        return x
 
-	def draw(self, screen):
-		pass
+    def get_collision_box(self):
+        return self.rect
+
+    def draw(self, screen):
+        
+        scaled_texture = pygame.transform.scale(myimage, (self.rect.length, self.rect.width))
+        screen.blit(scaled_texture, self.rect)
 
 class Test(Entity):
-	def __init__(self,x=0,y=0):
-		super().__init__(x,y)
-	def update(self, keystate, mouse_position, mouse_press, collided_entites):
-		self.rect.top = self.rect.top + 1
-		if keystate[K_UP]:
-			self.rect.top = self.rect.top - 2
-	def draw(self, screen):
-		pygame.draw.rect(screen, (200,200,200), self.rect)
+    def __init__(self,x=0,y=0):
+        super().__init__(x,y)
+    def update(self, keystate, mouse_position, mouse_press, collided_entites):
+        self.rect.top = self.rect.top + 1
+        if keystate[K_UP]:
+            self.rect.top = self.rect.top - 2
+    # def draw(self, screen):
+    #   pygame.draw.rect(screen, (200,200,200), self.rect)
